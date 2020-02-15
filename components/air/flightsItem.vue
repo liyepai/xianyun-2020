13d58cb329e43e985570bc25e4e1e7d563df44ce
liyepai <liyejiano@qq.com>
2020-02-15 16:58:18 +0800

    航班信息的组件

---
+++ components/air/flightsItem.vue
@@ -0,0 +1,197 @@
+<template>
+  <div class="flight-item">
+    <div @click="isShow = !isShow">
+      <!-- 显示的机票信息 -->
+      <el-row type="flex" align="middle" class="flight-info">
+        <el-col :span="6">
+          <span>{{ data.airline_name }}</span> {{ data.flight_no }}
+        </el-col>
+        <el-col :span="12">
+          <el-row
+            type="flex"
+            justify="space-between"
+            class="flight-info-center"
+          >
+            <el-col :span="8" class="flight-airport">
+              <strong>{{ data.dep_time }}</strong>
+              <span
+                >{{ data.org_airport_name }}{{ data.org_airport_quay }}</span
+              >
+            </el-col>
+            <el-col :span="8" class="flight-time">
+              <span>{{ returnTime }}</span>
+            </el-col>
+            <el-col :span="8" class="flight-airport">
+              <strong>{{ data.arr_time }}</strong>
+              <span
+                >{{ data.dst_airport_name }}{{ data.dst_airport_quay }}</span
+              >
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="6" class="flight-info-right">
+          ￥<span class="sell-price">{{ data.base_price / 2 }}</span
+          >起
+        </el-col>
+      </el-row>
+    </div>
+    <div class="flight-recommend" v-if="isShow">
+      <!-- 隐藏的座位信息列表 -->
+      <el-row type="flex" justify="space-between" align="middle">
+        <el-col :span="4">低价推荐</el-col>
+        <el-col :span="20">
+          <el-row
+            type="flex"
+            justify="space-between"
+            align="middle"
+            class="flight-sell"
+            v-for="(item, index) in data.seat_infos"
+            :key="index"
+          >
+            <el-col :span="16" class="flight-sell-left">
+              <span>{{ item.name }}</span> | {{ item.supplierName }}
+            </el-col>
+            <el-col :span="5" class="price"> ￥{{ item.settle_price }} </el-col>
+            <el-col :span="3" class="choose-button">
+              <el-button type="warning" size="mini">
+                选定
+              </el-button>
+              <p>剩余：{{ item.discount }}</p>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // 数据
+    data: {
+      type: Object,
+      // 默认是空数组
+      default: {}
+    }
+  },
+  data() {
+    return {
+      isShow: false
+    };
+  },
+  computed: {
+    returnTime() {
+      const end = this.data.arr_time.split(":"); // 出发时间 ["22", "50"]
+      const start = this.data.dep_time.split(":"); // 出发时间[20:30]
+      //先取出小时，再乘以一个小时60分钟 加上当前的分钟
+      // console.log(end); 取出来是一个数组 ["22", "50"]
+      //取出分钟 他们是字符串来的 小时*60 分钟，他会隐士转换成数字 后面的分钟则需要转换成数字类型
+      let endMM = end[0] * 60 + Number(end[1]);
+      let startMM = start[0] * 60 + Number(start[1]);
+
+      //如果到达时间小于开始，那么就是第二天的  得加上一天的分钟
+      if (endMM < startMM) {
+        endMM += 24 * 60;
+      }
+      //先求出相差多少分钟
+      const xc = endMM - startMM;
+      //在分别求出小时 分钟 然后拼接
+      //使用Math的方法进行取整
+      const xs=Math.floor(xc/60)
+      const fz =xc%60
+      return `${xs}小时${fz}分钟`
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.flight-item {
+  border: 1px #ddd solid;
+  margin-bottom: 10px;
+
+  .flight-info {
+    padding: 15px;
+    cursor: pointer;
+
+    > div {
+      &:first-child,
+      &:last-child {
+        text-align: center;
+      }
+    }
+  }
+
+  .flight-info-center {
+    padding: 0 30px;
+    text-align: center;
+
+    .flight-airport {
+      strong {
+        display: block;
+        font-size: 24px;
+        font-weight: normal;
+      }
+      span {
+        font-size: 12px;
+        color: #999;
+      }
+    }
+
+    .flight-time {
+      span {
+        display: inline-block;
+        padding: 10px 0;
+        border-bottom: 1px #eee solid;
+        color: #999;
+      }
+    }
+  }
+
+  .flight-info-right {
+    .sell-price {
+      font-size: 24px;
+      color: orange;
+      margin: 0 2px;
+    }
+  }
+}
+
+.flight-recommend {
+  background: #f6f6f6;
+  border-top: 1px #eee solid;
+  padding: 0 20px;
+
+  .flight-sell {
+    border-bottom: 1px #eee solid;
+    padding: 10px 0;
+
+    &:last-child {
+      border-bottom: none;
+    }
+
+    .flight-sell-left {
+      font-size: 12px;
+      span {
+        color: green;
+      }
+    }
+
+    .price {
+      font-size: 20px;
+      color: orange;
+    }
+
+    .choose-button {
+      text-align: center;
+      color: #666;
+      button {
+        display: block;
+        width: 100%;
+        margin-bottom: 5px;
+      }
+    }
+  }
+}
+</style>
